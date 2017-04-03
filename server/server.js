4d2fad430db06b2b041eebf37f1c2ca69e650796
Long Nguyen <litpuvn@gmail.com>
2017-04-03 13:56:08 -0500

    add log for details

--- server/server.js
+++ server/server.js
@@ -82,5 +82,5 @@ app.get('/', function (req, res) {
 
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('User study server is listening on port 3000!')
 });
