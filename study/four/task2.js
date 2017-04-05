20e7f49e270e4d050046cc3609c426ac17e99739
Long Nguyen <litpuvn@gmail.com>
2017-04-05 14:52:12 -0500

    adding task2 four

---
+++ study/four/task2.js
@@ -0,0 +1,196 @@
+var graphTypes = ['merged', 'multiple', 'horizon'];
+// level 200
+// 12x255; 45x43; 55x236
+
+
+// level above 600
+// 60x72, 54x59, 57x56
+var locations = [
+    [
+        {
+        lat: 57,
+        lon: 56,
+        sat: null
+        },
+        {
+            lat: 12,
+            lon: 255,
+            sat: null
+        },
+        {
+            lat: 55,
+            lon: 236,
+            sat: null
+        },
+        {
+            lat: 60,
+            lon: 72,
+            sat: null
+        },
+    ],
+    [
+        {
+            lat: 60,
+            lon: 72,
+            sat: null
+        },
+        {
+            lat: 118,
+            lon: 57,
+            sat: null
+        },
+        {
+            lat: 45,
+            lon: 53,
+            sat: null
+        },
+        {
+            lat: 12,
+            lon: 255,
+            sat: null
+        }
+    ],
+    [
+        {
+            lat: 60,
+            lon: 72,
+            sat: null
+        },
+        {
+            lat: 118,
+            lon: 57,
+            sat: null
+        },
+        {
+            lat: 55,
+            lon: 236,
+            sat: null
+        },
+        {
+            lat: 60,
+            lon: 72,
+            sat: null
+        }
+    ]
+];
+
+var startTime = (new Date()).getTime();
+
+var trialLocationIndex = (100 * Math.round(Math.random())) % (locations.length);
+var trialLocation = locations[trialLocationIndex];
+
+function nextLocations() {
+    trialLocationIndex = (100 * Math.round(Math.random())) % (locations.length);
+    trialLocation = locations[trialLocationIndex];
+}
+
+var queryParamUrl = 'lat1=' + trialLocation[0].lat + '&lon1=' + trialLocation[0].lon + '&lat2=' + trialLocation[1].lat + '&lon2=' + trialLocation[1].lon + '&lat3=' + trialLocation[2].lat + '&lon3=' + trialLocation[2].lon + '&lat4=' + trialLocation[3].lat + '&lon4=' + trialLocation[3].lon;
+var graphTypeToFile = {
+    merged: 'task2/ogallala.merged.3-years.html?' + queryParamUrl,
+    multiple: 'task2/ogallala.small.multiple.html?' + queryParamUrl,
+    horizon: 'task2/ogallala.horizon.single.surface.html?' + queryParamUrl
+};
+
+var currentQuestionIndex = -1;
+var currentGraphTypeIndex = 0;
+var graphType = graphTypes[currentGraphTypeIndex];
+
+
+function handleQuestionConfirm() {
+    // debugger;
+    if (currentQuestionIndex > -1) {
+        // handle logging
+        fireLogging(graphType);
+    }
+
+    currentQuestionIndex ++;
+    displayQuestion();
+
+}
+
+function nextGraphType() {
+    currentQuestionIndex = 0;
+    currentGraphTypeIndex ++;
+
+    if (currentGraphTypeIndex == graphTypes.length) {
+        alert("You are done");
+
+        return;
+    }
+
+
+    nextLocations();
+
+    queryParamUrl = 'lat1=' + trialLocation[0].lat + '&lon1=' + trialLocation[0].lon + '&lat2=' + trialLocation[1].lat + '&lon2=' + trialLocation[1].lon + '&lat3=' + trialLocation[2].lat + '&lon3=' + trialLocation[2].lon + '&lat4=' + trialLocation[3].lat + '&lon4=' + trialLocation[3].lon;
+    graphTypeToFile = {
+        merged: 'task2/ogallala.merged.3-years.html?' + queryParamUrl,
+        multiple: 'task2/ogallala.small.multiple.html?' + queryParamUrl,
+        horizon: 'task2/ogallala.horizon.single.surface.html?' + queryParamUrl
+    };
+
+    currentGraphTypeIndex = currentGraphTypeIndex % graphTypes.length;
+    graphType = graphTypes[currentGraphTypeIndex];
+
+    startTime = (new Date()).getTime();
+}
+
+function displayGraphType(graphType) {
+    if (!graphTypeToFile.hasOwnProperty(graphType)) {
+        alert("Not found fine representation for this graph type: " + graphType);
+        return;
+    }
+
+
+    d3.select("#graphType")
+        .text("Graph type: " + graphType);
+
+    var file = graphTypeToFile[graphType];
+    var iframe = document.getElementById('graph');
+    var src = iframe.getAttribute('src');
+    if (file != src) {
+        d3.select('#graph').attr('src', file);
+    }
+}
+
+function displayQuestion() {
+
+    if (currentQuestionIndex >= questionsAndAnswers.length) {
+
+        nextGraphType();
+    }
+
+
+    var questionAnswer =  questionsAndAnswers[currentQuestionIndex];
+
+    d3.select("#question")
+        .text(questionAnswer.question.text);
+
+
+    d3.select("#answer")
+        .selectAll('*')
+        .remove();
+
+    if (graphType == graphTypes[0]) {
+        d3.select("#answer").selectAll('span')
+            .data(questionAnswer.answer).enter()
+            .append('span')
+            .html(function (d) {
+                return '<input type="radio" name="answer" value="' + d.key  + '"/> <span style="color:' + d.color + '">'+ d.text + '</span><br/>';
+            });
+    }
+    else {
+        d3.select("#answer").selectAll('span')
+            .data(questionAnswer.answer).enter()
+            .append('span')
+            .html(function (d) {
+                return '<input type="radio" name="answer" value="' + d.key  + '"/>'+ d.text + '<br/>';
+            });
+    }
+
+    displayGraphType(graphType);
+
+}
+
+handleQuestionConfirm();
+
+
---
+++ study/four/task2/qas.js
@@ -0,0 +1,109 @@
+
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
+
+var httpURLBase = 'http://localhost:3000';
+var userId = guid();
+var cTen = d3.scale.category10();
+var colorRange = cTen.range();
+
+var questionsAndAnswers =  [
+        {
+            question: {
+                key: "t1q1",
+                text: "What year has highest saturated thickness?"
+            },
+            answer: [
+                {
+                    key: "2010",
+                    text:  "Year 2010 (yellow dot)",
+                    color: colorRange[0]
+
+                }
+                ,
+                {
+                    key: "2014",
+                    text:  "Year 2014 (red dot)",
+                    color: colorRange[1]
+
+                }
+                ,
+                {
+                    key: "2016",
+                    text:  "Year 2016 (green dot)",
+                    color: colorRange[2]
+
+                }
+            ]
+        }
+];
+
+
+function fireLogging(graphType) {
+
+
+    var graphTypeQuestions = questionsAndAnswers;
+
+    if (currentQuestionIndex >= graphTypeQuestions.length) {
+        console.log("Nothing to log. graph type: " + graphType + "; has invalid question index:" + currentQuestionIndex);
+        return;
+    }
+
+    var questionAnswer =  graphTypeQuestions[currentQuestionIndex];
+    var a = d3.select('#answer').select('input[name="answer"]:checked').node().value;
+    // d3.select('input[name="group-stack"]:checked').node().value;
+
+    var iframe = document.getElementById("graph");
+    var win = iframe.contentWindow;
+    var correct = win.maxYear.year == a;
+
+
+    var httpGetAsync = function httpGetAsync(theUrl, callback)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+
+                if (!!callback) {
+                    callback(xmlHttp.responseText);
+                }
+            }
+
+        };
+        xmlHttp.open("GET", theUrl, true); // true for asynchronous
+        xmlHttp.send(null);
+    };
+
+    var endTime = (new Date()).getTime();
+    var dur = endTime - startTime;
+    var maxStr = convertMaxYeartoQueryString(win.maxYear);
+    var myUrl = httpURLBase + '?user=' + userId + '&graphType=' + graphType + '&task=Task1&questionKey=' + questionAnswer.question.key + '&correct=' + correct + '&duration=' + dur + "&selected=" + a + maxStr;
+    httpGetAsync(myUrl);
+    console.log(questionAnswer);
+    console.log(a);
+}
+
+function convertMaxYeartoQueryString(maxYear) {
+    var str = '';
+    var val;
+    var yr;
+    for (i=1; i<= 10; i++) {
+        val =  "v" + i;
+        yr = "y" + i;
+        if (!maxYear.hasOwnProperty(val) || !maxYear.hasOwnProperty(yr)) {
+            console.log("Error, not found year " + yr + "; val:" + val);
+            break;
+        }
+        str += '&' + val + "=" + maxYear[val] + "&" + yr + "=" + maxYear[yr]
+    }
+
+
+    return str;
+}
